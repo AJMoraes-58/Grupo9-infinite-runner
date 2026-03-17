d056d68cda3727497b6310b104296a1e723630df
AJMoraes-58 <anajulia.dsmoraes.58@gmail.com>
2026-03-16 23:21:22 -0300

    feat: adiciona a corrida automatica e fixacao da camera no personagem principal

--- Scripts/forster.gd
+++ Scripts/forster.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 #var para vaiaveis; const para constantes
 #valor do pulo tem que ser negativo porque y=0 eh o topo da tela
 
+const VELOCIDADE_DA_CORRIDA: float = 300.0
 const VELOCIDADE_DO_PULO = -400
 var gravidade = ProjectSettings.get_setting("physics/2d/default_gravity") #valor padrao do godot
 
@@ -13,5 +14,5 @@ func _physics_process(delta: float) -> void: #loop para o jogo rodar
 	#ui_up eh a seta pra cima
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = VELOCIDADE_DO_PULO
-		
+	velocity.x = VELOCIDADE_DA_CORRIDA
 	move_and_slide() #movimenta o personagem
