2878bf2dcd2bfb721c635f92e3fff24d9d0f7d9d
AJMoraes-58 <anajulia.dsmoraes.58@gmail.com>
2026-03-18 12:14:49 -0300

    adiciona a mecanica de aceleracao do jogo conforme o tempo e dimiui a altura do segundo pulo como forma de balanceamento

--- Scripts/protagonista.gd
+++ Scripts/protagonista.gd
@@ -4,11 +4,14 @@ extends CharacterBody2D
 #var para vaiaveis; const para constantes
 #valor do pulo tem que ser negativo porque y=0 eh o topo da tela
 
-const VELOCIDADE_DA_CORRIDA: float = 300.0
-const VELOCIDADE_DO_PULO: float = -450.0
+const VELOCIDADE_DO_PRIMEIRO_PULO: float = -475.0
+const VELOCIDADE_DO_SEGUNDO_PULO: float = -370.0
 const MAXIMO_DE_PULOS: int = 2
-const ACELERADOR_DE_QUEDA: float = 2.0
+const VELOCIDADE_MAXIMA_DA_CORRIDA: float = 900.0
+const ACELERADOR_DE_QUEDA: float = 2.7
+const TAXA_DE_ACELERACAO_CORRIDA: float = 4.0
 
+var velocidadeDaCorrida: float = 300.0
 var gravidade = ProjectSettings.get_setting("physics/2d/default_gravity") #valor padrao do godot
 var quantidadeDePulos: int = 0
 
@@ -24,11 +27,15 @@ func _physics_process(delta: float) -> void: #loop para o jogo rodar
 	#ui_up eh a seta pra cima
 	if Input.is_action_just_pressed("ui_up") and quantidadeDePulos < MAXIMO_DE_PULOS:
 		if is_on_floor():
-			velocity.y = VELOCIDADE_DO_PULO
+			velocity.y = VELOCIDADE_DO_PRIMEIRO_PULO
 			quantidadeDePulos += 1
 
 		elif quantidadeDePulos < MAXIMO_DE_PULOS:
-			velocity.y = VELOCIDADE_DO_PULO
+			if quantidadeDePulos > 0:
+				velocity.y = VELOCIDADE_DO_SEGUNDO_PULO
+			else:
+				velocity.y = VELOCIDADE_DO_PRIMEIRO_PULO
+				
 			quantidadeDePulos += 1
 			animacao.stop()
 			animacao.play("pulo")
@@ -39,5 +46,8 @@ func _physics_process(delta: float) -> void: #loop para o jogo rodar
 	elif animacao.animation != "pulo":
 		animacao.play("pulo")
 		
-	velocity.x = VELOCIDADE_DA_CORRIDA
+	if velocidadeDaCorrida < VELOCIDADE_MAXIMA_DA_CORRIDA:
+		velocidadeDaCorrida += TAXA_DE_ACELERACAO_CORRIDA * delta
+	
+	velocity.x = velocidadeDaCorrida
 	move_and_slide()
