b860e35035ee3eebedfcccb72046e1bd61ddeb7d
AJMoraes-58 <anajulia.dsmoraes.58@gmail.com>
2026-03-19 19:59:41 -0300

    adiciona a funcionalidade de mutar a musica do jogo (a musica roda apenas no momento de gameplay)

--- Scripts/menu_principal.gd
+++ Scripts/menu_principal.gd
@@ -11,7 +11,8 @@ func _on_credito_pressed() -> void:
 	textoDeCreditos.visible = !textoDeCreditos.visible
 
 func _on_mutar_pressed() -> void:
-	pass
+	var somMusica = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_mute(somMusica, not AudioServer.is_bus_mute(somMusica))
 
 func _on_sair_pressed() -> void:
 	get_tree().quit()
