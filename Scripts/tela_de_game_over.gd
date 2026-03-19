edf45e69aa9341a5817fb611ccf9857ef4420cd3
AJMoraes-58 <anajulia.dsmoraes.58@gmail.com>
2026-03-19 16:01:36 -0300

    atualiza a tela de  game over com a opcao de voltar ao menu principal

--- Scripts/tela_de_game_over.gd
+++ Scripts/tela_de_game_over.gd
@@ -9,3 +9,7 @@ func Mostrar_Tela_Game_Over() -> void:
 func _on_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().reload_current_scene()
+
+func _on_ir_ao_menu_principal_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Cenas/menu_principal.tscn")
