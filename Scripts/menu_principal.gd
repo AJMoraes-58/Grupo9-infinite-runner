f83e29e4fb3fe3cd40ad80af446af20d32191f51
AJMoraes-58 <anajulia.dsmoraes.58@gmail.com>
2026-03-20 16:52:49 -0300

    adiciona a tela de creditos e seus botoes

--- Scripts/menu_principal.gd
+++ Scripts/menu_principal.gd
@@ -1,18 +1,10 @@
 extends Control
-@onready var textoDeCreditos = $textoDeCreditos
-
-func _ready() -> void:
-	textoDeCreditos.hide()
 	
 func _on_jogo_pressed() -> void:
 	get_tree().change_scene_to_file("res://Cenas/mundo.tscn")
 
 func _on_credito_pressed() -> void:
-	textoDeCreditos.visible = !textoDeCreditos.visible
-
-#func _on_mutar_pressed() -> void:
-	#var somMusica = AudioServer.get_bus_index("Master")
-	#AudioServer.set_bus_mute(somMusica, not AudioServer.is_bus_mute(somMusica))
+	get_tree().change_scene_to_file("res://Cenas/creditos.tscn")
 
 func _on_sair_pressed() -> void:
 	get_tree().quit()
