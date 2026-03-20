a922a2ee48d2ddbf659c665f02bda17dd812d009
AJMoraes-58 <anajulia.dsmoraes.58@gmail.com>
2026-03-20 17:03:46 -0300

    soluciona o bug da musica tocando por 1 seg ao voltar ao menu inicial

--- Scripts/tela_de_game_over.gd
+++ Scripts/tela_de_game_over.gd
@@ -11,5 +11,6 @@ func _on_button_pressed() -> void:
 	get_tree().reload_current_scene()
 
 func _on_ir_ao_menu_principal_pressed() -> void:
+	$"../AudioStreamPlayer".stop()
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Cenas/menu_principal.tscn")
